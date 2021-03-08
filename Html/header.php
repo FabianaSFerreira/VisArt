4247a498782d740ff67be953fdb905e85b1b789f
FabianaSFerreira <68201836+FabianaSFerreira@users.noreply.github.com>
2021-03-08 14:09:07 -0300

    Add files via upload

---
+++ Html/header.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="header">
+    <a class="navbar-brand" href="home.php"> <img src="Arquivos/VisArt/marca.png" width="150" height="50"> </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação"><span class="navbar-toggler-icon"></span></button>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item"> <a class="nav-link active" href="home.php">Home</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="galeria.php">Galeria</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="grupos.php">Grupos</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="eventos.php">Eventos</a> </li>
+            <li class="nav-item">
+                <?php 
+                    if ($usuario == "") { echo "<a class='nav-link' href='login.php'>Login</a>"; }
+                    else { echo "<a class='nav-link' href='perfil.php'>Perfil</a>"; }
+                ?>
+            </li>
+        </ul>
+        <form class="form-inline" id="buscar" action="galeria.php" method='post' align="rigth">
+            <input id="text_busca" type="text" name="texto" placeholder="Buscar artes" style="width: 90%;">
+            <button class="icon" type="submit" name="buscar" align="left" style="margin: 0; padding: 0; width: 10%;"> 
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                    <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                </svg>
+            </button> 
+        </form>  
+    </div>  
+</nav><br>
---
+++ Html/header_perfil.php
@@ -0,0 +1,49 @@
+<?php 
+    if ($_SESSION['IdPerfil'] != "") {
+        $perfil_usuario = mysqli_fetch_assoc(mySqli_query($conexao, "SELECT usuario, nome, email, LocalFoto FROM usuarios WHERE IdUsuario=".$_SESSION['IdPerfil'].""));
+
+        echo '<nav class="navbar navbar-expand-lg navbar-light bg-light" id="perfil">           
+                <div class="navbar-brand" id="img_perfil"> 
+                    <img src="'.$perfil_usuario['LocalFoto'].'" style="width:100%; height:100%;"><br> 
+                    <label style="margin: 0; padding: 10px; float: left;">'.$perfil_usuario['nome'].'</label>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarPerfil" aria-controls="navbarPerfil" aria-expanded="false" aria-label="Alterna navegação" style="margin: 0; padding: 10px; float: right;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/> </svg>
+                    </button>
+                </div>
+                    
+                <div class="collapse navbar-collapse" id="navbarPerfil" align="center">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item"> <a class="nav-link" href="minhas_artes.php">Portfólio</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="meus_grupos.php">Grupos</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="meus_grupos.php">Eventos</a> </li>
+                    </ul>
+                </div>  
+            </nav>';
+    }
+    else {
+        echo '<nav class="navbar navbar-expand-lg navbar-light bg-light" id="perfil">           
+                <div class="navbar-brand" id="img_perfil"> 
+                    <img src="'.$select['LocalFoto'].'" style="width:100%; height:100%;"><br>
+                    <label style="margin: 0; padding: 10px; float: left;">'.$select['nome'].'</label>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarPerfil" aria-controls="navbarPerfil" aria-expanded="false" aria-label="Alterna navegação" style="margin: 0; padding: 10px; float: right;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/> </svg>
+                    </button>
+                </div>
+                    
+                <div class="collapse navbar-collapse" id="navbarPerfil" align="center">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item"> <a class="nav-link" href="minhas_artes.php">Portfólio</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="meus_grupos.php">Grupos</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="meus_grupos.php">Eventos</a> </li><br>
+
+                        <li class="nav-item"> <form action="perfil.php" method="post"> <input  class="nav-link" id="nav_perfil" type="submit" name="modalArte" value="Adicionar Arte"></form> </li>
+                        <li class="nav-item"> <form action="perfil.php" method="post"> <input  class="nav-link" id="nav_perfil" type="submit" name="modalGrupo" value="Adicionar Grupo"></form> </li>
+                        <li class="nav-item"> <form action="perfil.php" method="post"> <input  class="nav-link" id="nav_perfil" type="submit" name="modalEvento" value="Adicionar Evento"></form> </li><br>
+                        
+                        <li class="nav-item"> <a class="nav-link" href="notificacao.php">Notificações</a> </li>                    
+                        <li class="nav-item"> <form action="perfil.php" method="post"> <input  class="nav-link" id="nav_perfil" type="submit" name="configuracoes" value="Configurações"></form> </li>
+                    </ul>
+                </div>  
+            </nav>';
+    }
+?>
